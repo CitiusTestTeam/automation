6e523c76a4b46df3f69c3f82598ed06cb5570437
Virus <Virus@Virus-PC>
2018-01-17 10:49:58 +0530

    App class removed

--- amazon/src/main/java/com/amazon/base/Singleton.java
+++ amazon/src/main/java/com/amazon/base/Singleton.java
@@ -5,7 +5,6 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.Parameters;
 
 import com.amazon.config.Config;
 import com.amazon.config.TestConfig;
