5dd196a22bca2349a1df1369039179394d618e4c
rajsha111 <rajatsh111@gmail.com>
2018-02-04 13:00:40 +0530

    commit1

---
+++ amazon/src/main/java/com/amazon/product/ProductDetails.java
@@ -0,0 +1,36 @@
+package com.amazon.product;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import com.amazon.base.Utility;
+import com.amazon.config.Config.RamSize;
+
+public class ProductDetails extends Utility{
+
+	public WebDriver driver;
+	private final String XPATH_TOTAL_SIZE_DETAIL = ".//*[@id='variation_size_name']/ul/li";
+	private final String XPATH_SIZE = ".//*[@id='size_name_%d']";
+	private final String XPATH_SELECTED_SIZE = ".//*[@id='variation_size_name']/div/span";
+
+	public ProductDetails(WebDriver driver){
+		super(driver);
+		this.driver = driver;
+		System.out.println("Switched window title: "+driver.getTitle());
+	}
+	
+	public void clickOnSize(RamSize ramsize){
+		 if(!getText(XPATH_SELECTED_SIZE).equals(ramsize)){
+			 List<WebElement> totalAvailableRamSize = elements(XPATH_TOTAL_SIZE_DETAIL);
+			 for(int i=0; i<totalAvailableRamSize.size(); i++){
+				 WebElement availableRamSizeElement = element(String.format(XPATH_SIZE, i));
+				 if(availableRamSizeElement.getText().equals(ramsize)){
+					 availableRamSizeElement.click();
+				 }
+			 }
+		 }
+		
+	}
+}
